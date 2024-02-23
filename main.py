f18fcd7c4fa1fd65100d9148268e22794b042408
Jackson Tabe <jackson.tabe@hotmail.com>
2024-02-23 20:47:03 +0100

    add run function and import wallet

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
 """
 This is the main script which is used for the Web3 portfolio analysis
 """
+import pandas as pd
+
+
+def run():
+    wallets_file = 'Data/Web3 wallets.xlsx'
+    wallets = pd.read_excel(wallets_file, sheet_name=['phantom', 'keplr', 'metamask', 'investment'])
+
+
+if __name__ == "__main__":
+    run()
