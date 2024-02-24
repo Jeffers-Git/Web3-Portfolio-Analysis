5cadcf3c627277653c7083482fd9f588c6cd2307
Jackson Tabe <jackson.tabe@hotmail.com>
2024-02-24 14:20:01 +0100

    add logging statements and get ready for metamask

--- main.py
+++ main.py
@@ -7,20 +7,26 @@ import metamask
 import phantom
 import sui
 import yaml
+import logging
 
 
 def run():
+    logging.basicConfig(level=logging.INFO, format='\033[92m%(asctime)s - %(levelname)s: %(message)s\033[0m')
+    logging.info('Maxu farmu activated...')
     with open('config.yml', 'r') as file:
         config = yaml.safe_load(file)
 
     wallets_file = 'data/Web3 wallets.xlsx'
-    wallets = pd.read_excel(wallets_file, sheet_name=['phantom', 'keplr', 'metamask', 'investment'])
+    wallets = pd.read_excel(wallets_file, sheet_name=['phantom', 'keplr', 'metamask', 'sui', 'investment'])
 
     # run analysis for the three wallets
+    logging.info("Running the analysis for all wallets...")
     phantom_data = phantom.run(data=wallets['phantom'])
     keplr_data = keplr.run(data=wallets['keplr'])
     metamask_data = metamask.run(data=wallets['metamask'])
     sui_data = sui.run(data=wallets['sui'])
+
+    logging.info('Maxu farmu ran succesfully...')
 
 
 if __name__ == "__main__":
--- metamask.py
+++ metamask.py
@@ -7,3 +7,4 @@ def run(data):
     # basic data cleaning
     data = data.dropna(axis=0, how='all')
     data = data.reset_index(drop=True)
+    data.columns = data.columns.str.lower()
