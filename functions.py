27ee099b08b6d86a132c55c7fd6895790fb07573
Jackson Tabe <jackson.tabe@hotmail.com>
2024-02-25 17:37:04 +0100

    make sure nan is not a ticker

--- functions.py
+++ functions.py
@@ -3,6 +3,7 @@ This script contains functions used in the analysis
 """
 import requests
 import pandas as pd
+import math
 
 
 def get_crypto_prices_coinmarketcap(data):
@@ -16,6 +17,7 @@ def get_crypto_prices_coinmarketcap(data):
     url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest'
 
     tickers = data[~data['ticker'].str.contains('-')]['ticker'].unique()
+    tickers = [x for x in tickers if not (isinstance(x, float) and math.isnan(x))]
     prices = {}
     for ticker in tickers:
         params = {
@@ -38,7 +40,8 @@ def get_crypto_prices_coingecko(data):
     # Coingecko API
     api = "https://api.coingecko.com/api/v3/simple/price"
     crypto_mapping = pd.read_csv('tables/crypto_mapping.csv')
-    tickers = data[~data['ticker'].str.contains('-|usdt|usdc|jitosol|jup')]['ticker'].unique()
+    tickers = data['ticker'].unique()
+    tickers = [x for x in tickers if not (isinstance(x, float) and math.isnan(x))]
     prices = {}
     for ticker in tickers:
         id = crypto_mapping[crypto_mapping['ticker'] == ticker]['id'].iloc[0]
@@ -51,8 +54,6 @@ def get_crypto_prices_coingecko(data):
         price = coingecko[id]['usd']
         prices[ticker] = price
 
-    prices['usdc'] = 1
-    prices['usdt'] = 1
     data['prices'] = data['ticker'].map(prices)
     data['value'] = data['amount'] * data['prices']
 
