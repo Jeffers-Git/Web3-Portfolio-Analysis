ab465cacdb5ace7151da7362f122b330761e36fb
Jackson Tabe <jackson.tabe@hotmail.com>
2024-02-25 17:37:44 +0100

    update metamask and sui

--- metamask.py
+++ metamask.py
@@ -1,6 +1,7 @@
 """
 This script runs the analysis for the metamask wallet
 """
+from functions import get_crypto_prices_coingecko
 
 
 def run(data):
@@ -8,3 +9,14 @@ def run(data):
     data = data.dropna(axis=0, how='all')
     data = data.reset_index(drop=True)
     data.columns = data.columns.str.lower()
+    data['ticker'] = data['ticker'].str.upper()
+
+    data = get_crypto_prices_coingecko(data)
+
+    value_per_dapp = data.groupby(['app'])['value'].sum()
+    # exclude total in case of rerun in debug
+    value_per_dapp['Total'] = value_per_dapp[value_per_dapp.index != 'Total'].sum()
+    value_per_dapp = value_per_dapp.round(2)
+    value_per_dapp.to_csv('results/metamask/value_per_dapp.csv')
+
+    return data
--- sui.py
+++ sui.py
@@ -1,7 +1,21 @@
 """
 This script runs the analysis for the sui wallet
 """
+from functions import get_crypto_prices_coinmarketcap
 
+def run(data):
+    # basic data cleaning
+    data = data.dropna(axis=0, how='all')
+    data = data.reset_index(drop=True)
+    data.columns = data.columns.str.lower()
+    data['ticker'] = data['ticker'].str.upper()
 
-def run():
-    pass
+    data = get_crypto_prices_coinmarketcap(data)
+
+    value_per_dapp = data.groupby(['app'])['value'].sum()
+    # exclude total in case of rerun in debug
+    value_per_dapp['Total'] = value_per_dapp[value_per_dapp.index != 'Total'].sum()
+    value_per_dapp = value_per_dapp.round(2)
+    value_per_dapp.to_csv('results/sui/value_per_dapp.csv')
+
+    return data
