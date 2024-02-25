93e8b4c6f31ce2bab47b702f18105bea00167474
Jackson Tabe <jackson.tabe@hotmail.com>
2024-02-25 19:02:19 +0100

    do not include where value is 0

--- phantom.py
+++ phantom.py
@@ -17,7 +17,14 @@ def run(data):
     value_per_dapp = data.groupby(['app'])['value'].sum()
     # exclude total in case of rerun in debug
     value_per_dapp['Total'] = value_per_dapp[value_per_dapp.index != 'Total'].sum()
+    value_per_dapp = value_per_dapp[value_per_dapp != 0]
     value_per_dapp = value_per_dapp.round(2)
     value_per_dapp.to_csv('results/phantom/value_per_dapp.csv')
+
+    value_per_purpose = data.groupby(['purpose'])['value'].sum()
+    value_per_purpose['Total'] = value_per_purpose[value_per_purpose.index != 'Total'].sum()
+    value_per_purpose = value_per_purpose[value_per_purpose != 0]
+    value_per_purpose = value_per_purpose.round(2)
+    value_per_purpose.to_csv('results/phantom/value_per_purpose.csv')
 
     return data
